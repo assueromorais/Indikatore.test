7af2759f37e42ea75d809cfa90d392900179c6aa
ASSUÉRO ARAÚJO MORAIS <assueromorais@hotmail.com>
2018-02-16 23:17:22 -0200

    Tratamento de argumento recebido via console.

--- Indikatore.Teste/Program.fs
+++ Indikatore.Teste/Program.fs
@@ -4,12 +4,22 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Jogo FizzBuzz digital"
-    printfn "Informe o número final do contador:"
-    let x = Console.ReadLine()  
-    if x <> null then
-        let max = x |> Int32.Parse
-        FizzBuzzJogo.FizzBuzz max
+    let mutable x = ""
+    let mutable max = 0
+    if (argv.Length > 0) then
+        Int32.TryParse(argv.[0], &max) |> ignore
 
+    if max = 0 then
+        printfn "Jogo FizzBuzz digital"
+        printfn "Informe o número final do contador:"
+        x <- Console.ReadLine()
+        if x <> null then
+            Int32.TryParse(x, &max) |> ignore
+
+    if max > 0 then
+        printfn "Contando até %i" max
+        FizzBuzzJogo.FizzBuzz max
+    
+    printfn "Digite qualquer tecla para fechar..."
     Console.ReadKey() |> ignore
     0  
